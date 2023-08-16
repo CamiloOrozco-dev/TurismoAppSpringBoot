41fbda78a6884809a5e8303ce57ad61ec684af08
Manuc6 <133366855+Manuc6@users.noreply.github.com>
2023-08-16 12:24:32 -0500

    Create repository, class offer and company

---
+++ src/main/java/com/example/TurismoApp/models/Company.java
@@ -0,0 +1,14 @@
+package com.example.TurismoApp.models;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+public class Company {
+
+    private Integer id;
+    private String nit;
+    private String name;
+    private Integer location;
+    private String description;
+
+}
---
+++ src/main/java/com/example/TurismoApp/models/Offer.java
@@ -0,0 +1,12 @@
+package com.example.TurismoApp.models;
+
+import java.time.LocalDate;
+
+public class Offer {
+    private Integer id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Double personCost;
+}
